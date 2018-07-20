6a2f54e68ae9abf506fdd2f663d8f088f42a6a87
Andreas Zweili <andreas@2li.ch>
2018-07-20 08:40:39 +0200

    extend the setup file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt install -y inkscape texlive-full emacs graphviz
 WORKING_DIRECTORY=$(pwd)
 ln -sf $WORKING_DIRECTORY/general $WORKING_DIRECTORY/projektantrag/
 ln -sf $WORKING_DIRECTORY/general $WORKING_DIRECTORY/projekthandbuch/
